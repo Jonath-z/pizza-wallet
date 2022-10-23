f6a0c97699aef71232d6843a2615b5b9b953a496
Jonath-z <jonathanzihindula95@gmail.com>
2022-10-23 15:02:04 +0200

    feat: QR scanner component added

--- src/components/QrReaderModal.jsx
+++ src/components/QrReaderModal.jsx
@@ -3,9 +3,21 @@ import { QrReader } from "react-qr-reader";
 import useConnectDapp from "../hooks/useConnectDapp";
 
 const style = {
-  width: "500px",
-  height: "500px",
-  margin: "auto",
+  container: {
+    width: "500px",
+    height: "500px",
+    margin: "auto",
+    padding: "10px",
+    borderRadius: "10px",
+    border: "2px dashed white",
+  },
+
+  videoStyle: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    borderRadius: "10px",
+  },
 };
 
 const QrReaderModal = () => {
@@ -20,11 +32,12 @@ const QrReaderModal = () => {
   };
 
   return (
-    <div style={style}>
+    <div style={style.container}>
       <QrReader
         onResult={handleQrResult}
         scanDelay={300}
         videoContainerStyle={style}
+        videoStyle={style.videoStyle}
       />
     </div>
   );
